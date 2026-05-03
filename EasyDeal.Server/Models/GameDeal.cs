405aaca1638c45c83541409e3f8efbbe372088ef
Caleb <calebn8010@gmail.com>
2026-05-03 14:00:35 -0400

    Fix AddDeal bug

--- EasyDeal.Server/Models/GameDeal.cs
+++ EasyDeal.Server/Models/GameDeal.cs
@@ -3,7 +3,7 @@ namespace EasyDeal.Server.Models
     public class GameDeal
     {
         public string gameID { get; set; }
-        public string steamAppID { get; set; }
+        public string? steamAppID { get; set; } //Only field that can be null, causing issues for deluxe game deal entries - sending client 400 reponse
         public string cheapest { get; set; }
         public string cheapestDealID { get; set; }
         public string external { get; set; }
