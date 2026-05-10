2bac032ab6b6e3f97dc8d5d1bae2e5d6267acff4
Caleb <calebn8010@gmail.com>
2026-05-10 13:15:06 -0400

    Azure deploy fix / More verbose notifications

--- easydeal.client/src/Pages/Home.tsx
+++ easydeal.client/src/Pages/Home.tsx
@@ -41,7 +41,7 @@ function Home() {
         if (response.status === 409) {
             showNotification('Game already in your Wishlist ✓', '#facc15', 6000);
         } else if (response.status === 200) {
-            showNotification('Added to Wishlist ✓', '#22c55e', 3500);
+            showNotification(`Added to Wishlist ✓ ${deal?.external}`, '#22c55e', 3500);
         } else {
             showNotification('Add to Wishlist unsuccessful, please try again later.', '#ef4444', 3500);
         }
@@ -85,7 +85,7 @@ function Home() {
             });
             if (response.ok) {
                 setWishlistItems(prev => prev.filter((_item, i) => i !== index));
-                showNotification('Removed from Wishlist', '#64748b', 2500);
+                showNotification(`Removed from Wishlist: ${item.external}`, '#64748b', 2500);
             } else {
                 showNotification('Could not remove item, please try again.', '#ef4444', 3500);
             }
