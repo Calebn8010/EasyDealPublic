0d3a2a555aedfa577e4d5f916ab467f60df7bb8d
Caleb <calebn8010@gmail.com>
2026-05-13 16:56:30 -0400

    Initial commit

---
+++ EasyDeal.Server/Data/WishlistAlert.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EasyDeal.Server.Data;
+
+namespace EasyDeal.Server.Data
+{
+    public class WishlistAlert
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string GameName { get; set; }
+
+        public string GameId { get; set; }
+
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+
+        public DateTime DateSetInactive { get; set; } = DateTime.UtcNow;
+
+        public DateTime DateDeleted { get; set; } = DateTime.UtcNow;
+
+        public string UserId { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsDeleted { get; set; }
+        //public ApplicationUser User { get; set; }
+    }
+}
+
---
+++ EasyDeal.Server/Models/WishlistAlert.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyDeal.Server.Models
+{
+    public class WishlistAlert
+    {
+        public string gameID { get; set; }
+        public string external { get; set; }
+        public string targetPrice { get; set; }
+
+    }
+}
