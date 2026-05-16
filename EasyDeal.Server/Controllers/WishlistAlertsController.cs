1cf35cb3c0dafdb995f950d29093b6e54018a45c
Caleb <calebn8010@gmail.com>
2026-05-16 12:24:41 -0400

    Initial working

--- EasyDeal.Server/Controllers/WishlistAlertsController.cs
+++ EasyDeal.Server/Controllers/WishlistAlertsController.cs
@@ -27,7 +27,7 @@ namespace EasyDeal.Server.Controllers
 
         // Handles Post request from front end for WishlistUpdates endpoint
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] GameDeal request)
+        public async Task<IActionResult> Post([FromBody] WishlistGameAlert request)
         {
             _logger.LogInformation("WishlistAlertsController Post method called.");
 
@@ -41,11 +41,11 @@ namespace EasyDeal.Server.Controllers
 
             _logger.LogInformation($"User id: {userId}");
 
-            return Ok();
+            //return Ok();
 
-            /*
+            
 
-            WishlistAlertResult status_result = AddToAlerts(request, userId);
+            WishlistAlertResult status_result = AddToWishlistAlerts(request, userId);
 
             return status_result switch
             {
@@ -54,8 +54,80 @@ namespace EasyDeal.Server.Controllers
                 _ => BadRequest()
             };
 
-            */
+            
         }
 
+        private WishlistAlertResult AddToWishlistAlerts(WishlistGameAlert game, string userid)
+        {
+            _logger.LogInformation($"Game deal to add to WishlistAlerts: {game.external}");
+            // Implement logic to add the game deal to the wishlist
+
+            //check if WishlistAlert record with user id is already in db
+            List<WishlistAlert> wishlist_game = _context.WishlistAlerts
+                .Where(w => w.GameId == game.gameId && w.UserId == userid)
+                .ToList();
+
+            _logger.LogInformation($"wishlist game info: {wishlist_game}");
+
+            // Already exisits for user / gameID, set IsActive false before adding new game Alert record
+            if (wishlist_game.Count > 0)
+            {
+                wishlist_game[0].IsActive = false;
+                _context.SaveChanges();
+            }
+
+            // Map GameDeal to Wishlist
+            WishlistAlert wishlistAlertEntry = new WishlistAlert
+            {
+                GameName = game.external,
+                GameId = game.gameId,
+                TargetPrice = game.targetPrice,
+                DateAdded = DateTime.UtcNow,
+                UserId = userid,
+                IsActive = true
+                // IsDeleted defaults to false since it is a bool
+            };
+
+            _context.WishlistAlerts.Add(wishlistAlertEntry);
+            int result = _context.SaveChanges();
+
+            if (result == 0)
+            {
+                _logger.LogError($"Not able to save changes - check db connection");
+                return WishlistAlertResult.DatabaseError;
+            }
+
+            _logger.LogInformation($"New wishlist add saved successfully");
+            return WishlistAlertResult.Success; // returns as success if at least one row was affected
+        }
+
+
+
+        private WishlistAlertResult CreateNewWishlistAlert(WishlistGameAlert game, string userid)
+        {
+            // Map GameDeal to Wishlist
+            WishlistAlert wishlistAlertEntry = new WishlistAlert
+            {
+                GameName = game.external,
+                GameId = game.gameId,
+                TargetPrice = game.targetPrice,
+                DateAdded = DateTime.UtcNow,
+                UserId = userid,
+                IsActive = true
+                // IsDeleted defaults to false since it is a bool
+            };
+
+            _context.WishlistAlerts.Add(wishlistAlertEntry);
+            int result = _context.SaveChanges();
+
+            if (result == 0)
+            {
+                _logger.LogError($"Not able to save changes - check db connection");
+                return WishlistAlertResult.DatabaseError;
+            }
+
+            _logger.LogInformation($"New wishlist add saved successfully");
+            return WishlistAlertResult.Success; // returns as success if at least one row was affected
+        }
     }
 }
