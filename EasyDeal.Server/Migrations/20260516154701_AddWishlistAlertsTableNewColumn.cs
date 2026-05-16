1ffe7c541aa790716f3efd5b10f0ead64130297a
Caleb <calebn8010@gmail.com>
2026-05-16 12:09:46 -0400

    Add new column for WishlistAlerts

---
+++ EasyDeal.Server/Migrations/20260516154701_AddWishlistAlertsTableNewColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyDeal.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWishlistAlertsTableNewColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TargetPrice",
+                table: "WishlistAlerts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TargetPrice",
+                table: "WishlistAlerts");
+        }
+    }
+}
