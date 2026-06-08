1bb1501fac83fe35a7eb6a90271aeb62bb860a83
Caleb <calebn8010@gmail.com>
2026-06-07 21:55:23 -0400

    Initial commit

---
+++ easydeal.client/src/Components/GameBackground.tsx
@@ -0,0 +1,11 @@
+// GameBackground.tsx
+import "../GameBackground.css";
+
+export default function GameBackground() {
+    return (
+        <div className="game-bg-wrapper">
+            <div className="game-bg-image" />
+            <div className="game-bg-overlay" />
+        </div>
+    );
+}
