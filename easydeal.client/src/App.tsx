0b9f6f71fccc8195dc13a0f082e5289db397ace1
Caleb <calebn8010@gmail.com>
2026-06-13 11:29:23 -0400

    Add Landing page and new styling

--- easydeal.client/src/App.tsx
+++ easydeal.client/src/App.tsx
@@ -1,21 +1,23 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Landing from './Pages/Landing.tsx';
 import Home from './Pages/Home.tsx';
 import Login from './Pages/Login.tsx';
 import Register from './Pages/Register.tsx';
-
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Landing />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </BrowserRouter>
     );
-
 }
+
 export default App;
