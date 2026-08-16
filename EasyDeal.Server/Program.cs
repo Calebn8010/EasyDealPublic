0c3f1ba3c4d6b111536ec1a92656be771e39eb26
Caleb <calebn8010@gmail.com>
2026-08-16 09:22:32 -0400

    Add for azure logging

--- EasyDeal.Server/Program.cs
+++ EasyDeal.Server/Program.cs
@@ -13,6 +13,9 @@ namespace EasyDeal.Server.Data
 
             var builder = WebApplication.CreateBuilder(args);
 
+            //Add in for azure logging
+            builder.Logging.AddAzureWebAppDiagnostics();
+
             var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
